6f0b7a9095fe29f3780afd1a407cb70380f59c40
Tomas Krehlik <tomas.krehlik@gmail.com>
2015-05-11 00:22:14 +0200

    Remove greeks

--- R/fevd.R
+++ R/fevd.R
@@ -39,16 +39,16 @@ fevd <- function(est, n.ahead = 100) {
 #' @author Tomas Krehlik \email{tomas.krehlik@gmail.com}
 
 fftFEVD <- function(est, n.ahead = 100) {
-	Φ <- irf(est, n.ahead = n.ahead, boot = F, ortho = F)
+	Phi <- irf(est, n.ahead = n.ahead, boot = F, ortho = F)
 
-	fftir <- lapply(Φ$irf, function(i) apply(i, 2, fft))
+	fftir <- lapply(Phi$irf, function(i) apply(i, 2, fft))
 	fftir <- lapply(1:(n.ahead+1), function(j) sapply(fftir, function(i) i[j,]))
 
-	Φ <- lapply(1:(n.ahead + 1), function(j) sapply(Φ$irf, function(i) i[j,]))
-	Σ <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
+	Phi <- lapply(1:(n.ahead + 1), function(j) sapply(Phi$irf, function(i) i[j,]))
+	Sigma <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
 	
-	denom <- diag(Reduce('+', lapply(Φ, function(i) i %*% Σ %*% t(i) )))
-	enum <- lapply(fftir, function(i) (abs(i%*%t(chol(Σ))))^2/(n.ahead+1))
+	denom <- diag(Reduce('+', lapply(Phi, function(i) i %*% Sigma %*% t(i) )))
+	enum <- lapply(fftir, function(i) (abs(i%*%t(chol(Sigma))))^2/(n.ahead+1))
 	a <- lapply(enum, function(i) t(sapply(1:est$K, function(j) i[j,]/(denom[j]))))
 
 	return(a)
@@ -67,15 +67,15 @@ fftFEVD <- function(est, n.ahead = 100) {
 #' @author Tomas Krehlik \email{tomas.krehlik@gmail.com}
 
 genFEVD <- function(est, n.ahead = 100) {
-	Φ <- irf(est, n.ahead = n.ahead+1, boot = F, ortho = F)
-	Φ <- lapply(1:(n.ahead + 1), function(j) sapply(Φ$irf, function(i) i[j,]))
-	Σ <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
+	Phi <- irf(est, n.ahead = n.ahead+1, boot = F, ortho = F)
+	Phi <- lapply(1:(n.ahead + 1), function(j) sapply(Phi$irf, function(i) i[j,]))
+	Sigma <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
 	
-	denom <- diag(Reduce('+', lapply(Φ, function(i) i %*% Σ %*% t(i) )))
-	enum <- Reduce('+', lapply(Φ, function(i) (i%*%Σ)^2))
+	denom <- diag(Reduce('+', lapply(Phi, function(i) i %*% Sigma %*% t(i) )))
+	enum <- Reduce('+', lapply(Phi, function(i) (i%*%Sigma)^2))
 	# print(enum)
 	# print(denom)
-	a <- sapply(1:est$K, function(j) enum[j,]/(denom[j]*sqrt(diag(Σ))))
+	a <- sapply(1:est$K, function(j) enum[j,]/(denom[j]*sqrt(diag(Sigma))))
 	# print(a)
 	a <- t(apply(a, 2, function(i) i / sum(i) ))
 	return(a)
@@ -94,17 +94,17 @@ genFEVD <- function(est, n.ahead = 100) {
 #' @author Tomas Krehlik \email{tomas.krehlik@gmail.com}
 
 fftGenFEVD <- function(est, n.ahead = 100) {
-	Φ <- irf(est, n.ahead = n.ahead, boot = F, ortho = F)
+	Phi <- irf(est, n.ahead = n.ahead, boot = F, ortho = F)
 
-	fftir <- lapply(Φ$irf, function(i) apply(i, 2, fft))
+	fftir <- lapply(Phi$irf, function(i) apply(i, 2, fft))
 	fftir <- lapply(1:(n.ahead+1), function(j) sapply(fftir, function(i) i[j,]))
 
-	Φ <- lapply(1:(n.ahead + 1), function(j) sapply(Φ$irf, function(i) i[j,]))
-	Σ <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
+	Phi <- lapply(1:(n.ahead + 1), function(j) sapply(Phi$irf, function(i) i[j,]))
+	Sigma <- (t(sapply(est$varresult, function(i) i$residuals)) %*% sapply(est$varresult, function(i) i$residuals))/nrow(sapply(est$varresult, function(i) i$residuals))
 	
-	denom <- diag(Reduce('+', lapply(Φ, function(i) i %*% Σ %*% t(i) )))
-	enum <- lapply(fftir, function(i) (abs(i%*%Σ))^2/(n.ahead+1))
-	a <- lapply(enum, function(i) sapply(1:est$K, function(j) i[j,]/(denom[j]*sqrt(diag(Σ)))))
+	denom <- diag(Reduce('+', lapply(Phi, function(i) i %*% Sigma %*% t(i) )))
+	enum <- lapply(fftir, function(i) (abs(i%*%Sigma))^2/(n.ahead+1))
+	a <- lapply(enum, function(i) sapply(1:est$K, function(j) i[j,]/(denom[j]*sqrt(diag(Sigma)))))
 	tot <- apply(Reduce('+', a), 2, sum)
 
 	a <- lapply(a, function(i) t(i)/tot)
